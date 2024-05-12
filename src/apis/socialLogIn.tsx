db03370a937a49fdb82e2d9ef818cb6564687d4a
sebin32 <sebin6472@hanmail.net>
2024-05-12 22:54:30 +0900

    upload after modifying https to http

--- src/apis/socialLogIn.tsx
+++ src/apis/socialLogIn.tsx
@@ -1,6 +1,6 @@
-const VITE_API_URL_SOCIAL_GOOGLE = 'https://snapevent.site/oauth2/authorization/google';
-const VITE_API_URL_SOCIAL_NAVER = 'https://snapevent.site/oauth2/authorization/naver';
-const VITE_API_URL_SOCIAL_KAKAO = 'https://snapevent.site/oauth2/authorization/kakao';
+const VITE_API_URL_SOCIAL_GOOGLE = 'http://snapevent.site/oauth2/authorization/google';
+const VITE_API_URL_SOCIAL_NAVER = 'http://snapevent.site/oauth2/authorization/naver';
+const VITE_API_URL_SOCIAL_KAKAO = 'http://snapevent.site/oauth2/authorization/kakao';
 type fetchData= () => void;
 const socialLogIn : fetchData =  () => {
     fetch(VITE_API_URL_SOCIAL_GOOGLE);
